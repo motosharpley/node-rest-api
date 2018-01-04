471a41610d1e30ddb7ca89b7cc07112d8a48061d
motosharpley <motosharpley@gmail.com>
2018-01-04 13:47:38 -0600

    added nodemon, routes, product resource

---
+++ api/routes/products.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello /product GET request!'
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Hello /product POST request!'
+  });
+});
+
+router.get('/:productId', (req, res, next) => {
+  const id = req.params.productId;
+  if (id === 'special') {
+    res.status(200).json({
+      message: 'This produt is on special',
+      id: id
+    });
+  } else {
+    res.status(200).json({
+      message: 'You passed an id'
+    });
+  }
+});
+
+module.exports = router;
