79542542a553c1d81e208268d836ad1960988a93
motosharpley <motosharpley@gmail.com>
2018-01-04 16:32:12 -0600

    add body parser and cors headers

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  };
   res.status(201).json({
-    message: 'Hello /product POST request!'
+    message: 'Hello /product POST request!',
+    createdProduct: product
   });
 });
 
