3066a42356d6fb5fc51a4a5cad64d60b8f50fa56
motosharpley <motosharpley@gmail.com>
2018-01-05 13:09:32 -0600

    add mongoose set up product crud roustes

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const configDB = require('./config/database');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+// connect to mongoDB
+mongoose.connect(configDB.database);
+const db = mongoose.connection;
+// Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+});
+// Check for db errors
+db.on('error', function(err){
+    console.log(err);
+});
 
 // middlewares
 app.use(morgan('dev'));
